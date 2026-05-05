41254b509c44a41012e94c45ecc551e112817dcb
Tim <tim@local.ubt>
2026-05-05 14:39:30 +0200

    Initial commit - java service setup

---
+++ trivia-service/src/main/java/com/timbruyn/trivia_service/TriviaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.timbruyn.trivia_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriviaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriviaServiceApplication.class, args);
+	}
+
+}
